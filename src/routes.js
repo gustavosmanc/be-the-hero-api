7f3e3b2b1475c9808fd456412108280aecabcd6d
gustavosmanc <gustavosmanc@gmail.com>
2020-03-29 00:08:31 -0300

    API Be The Hero Node.JS

---
+++ src/routes.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const { celebrate, Segments, Joi } = require("celebrate");
+
+const SessionControler = require("./controllers/SessionController");
+const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+
+const routes = express.Router();
+
+routes.post("/sessions", SessionControler.create);
+
+routes.get("/ongs", OngController.index);
+
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.number()
+        .required()
+        .min(1000000000)
+        .max(99999999999),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  OngController.create
+);
+
+routes.get(
+  "/incidents",
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }),
+  IncidentController.index
+);
+
+routes.post("/incidents", IncidentController.create);
+
+routes.delete(
+  "/incidents/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required()
+    })
+  }),
+  IncidentController.delete
+);
+
+routes.get(
+  "/profile",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  ProfileController.index
+);
+
+module.exports = routes;
